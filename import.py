6966d4420ca3e484e29bac28cdf6af2ad786621b
CarreenWu <carreenw@nycstudents.net>
2024-09-13 09:21:40 -0400

    pt2

--- import.py
+++ import.py
@@ -2,7 +2,38 @@
 y = float(3)
 print(x,y) """
 
-values = [1,2.23,5,7,2,30,15]
+""" values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
-    print(i)
+    print(i) """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = "test"
+print(f"hello {x}") """
+
+""" temp = 50
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+""" x=13
+if 0 == x%2:
+    print('even')
+else:
+    print('odd') """
+
+bill = okay
